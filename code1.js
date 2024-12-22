aa725fa544ac54ee08c3be1833a9cd8ce071a007
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2024-12-22 22:41:08 +0530

    js part 2

--- code1.js
+++ code1.js
@@ -1,1 +1,127 @@
-console.log("Hello");
+// console.log("Hello");
+
+// 1 comments    -> //        or /* */
+
+// 2 Data Types
+  /*
+     undefined
+     null
+     boolean
+     string
+     symbol
+     number
+     object
+  */
+
+ //  3 Variables
+   /*
+      var -> Will be usd throughout thr whole program
+      let -> only in scope where variable is declared
+      const -> variables that shoul never change
+  */
+
+// 4
+ /*
+     var a ;
+     var b = 2 ;
+ */
+
+
+// 5    Summation
+     /*
+ var a , b , c;
+ a = 2;
+ b = 3;
+ c = 2;
+ console.log(a+b+c);
+ */
+
+
+ // variable names case sensitive
+
+// 6 Float
+ // var x = 2.00  
+
+
+// 7 printing quotes using escape character
+
+ /*
+var str = "HEllo \"quotes\"";
+console.log(str);                     => ANOTHER METHOD IS TO USE SINGLE QUOTES
+ */
+
+// ***   what if i want to use both quotes -> use backticks -> ` Write Anything Here    `
+
+
+// 8 Escape Chracters
+ /*
+    \' --  single quote
+    \"" -- Double quote
+    \\ -- backslash
+    \n -- newline
+    \r --
+    \t -- tab
+    \b -- backspace
+    \f -- 
+ */
+
+// concatentaio
+
+ /*
+var k = "hi";
+var r = "hello";
+console.log(k + " "+ r);
+ */
+
+
+// 9 append
+
+// var s = "245";
+// s.append("3");         => Wrong no append method
+// console.log(s);
+
+
+// 10  length
+// name.length;
+
+// 11 string immutability
+
+ /*
+  var s = "hello";
+  s[0] = "h"  =? can not change a single character
+ */
+
+//12 Array
+ //  **** In JS  an array can hold elements of diff data types also
+ // []  -> braces for array
+
+ /*
+ var a= ["hello" , 39 , "kg" ];
+ console.log(a[1]);
+ */
+
+ // 13 Nested Array
+
+  // var x = [ [a ,2] , [b , 6] ];
+
+  
+// 14   push  ans pop  in array
+
+// var arr = ["roshni" , "kartikeya" , 12];
+// console.log(arr[2]);
+// arr.push("Hello");
+// console.log(arr[3]);
+// arr.pop();
+// console.log(arr.length)
+
+
+//15  shift
+// removes first element
+
+//  16 unshift   pushes element in front
+
+
+// 17 FUNCTIONS
+
+
+
